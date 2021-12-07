cc86720b52ca759429f60ded0ef86f50d8149db4
Ronaldo Vargas <ronaldovh99@gmail.com>
2021-12-06 23:27:23 -0500

    Initial Commit

---
+++ POS/POSApp.swift
@@ -0,0 +1,20 @@
+//
+//  POSApp.swift
+//  POS
+//
+//  Created by Ronaldo Andre Vargas Huaman on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct POSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
